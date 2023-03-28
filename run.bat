96bd3dd56a46c443233632a04f4685758a1e3837
ThangLC304 <thang.luongcao@gmail.com>
2023-03-28 13:43:03 +0800

    setup & run OS compatibility update
    setup and run files are now compatible on Windows, macOS and Linux

--- run.bat
+++ run.bat
@@ -5,26 +5,34 @@ set venv_name=TAN_env
 REM set TAN_dir to directory of this file
 set "TAN_dir=%~dp0"
 
-if exist "%UserProfile%\anaconda3" (
-  set "conda_path=%UserProfile%\anaconda3"
-) else (
+if "%OS%"=="Windows_NT" (
   set "conda_path=%UserProfile%\miniconda3"
+  set "activate_cmd=call %conda_path%\Scripts\activate.bat %venv_name%"
+) else (
+  set "conda_path=$HOME/miniconda3"
+  set "activate_cmd=source $conda_path/bin/activate %venv_name%"
 )
-
 
 if exist "%conda_path%\envs\%venv_name%" (
     echo %venv_name% already exists
 ) else (
     echo %venv_name% not found, please run setup.bat again
-    pause
+    if "%OS%"=="Windows_NT" (
+      set /p _=Press any key to continue...
+    ) else (
+      read -p "Press [Enter] key to continue..."
+    )
     exit 
     )
 
 REM activate TAN environment
-echo Activating virtual environment... at %conda_path%\envs\%venv_name%
-call %conda_path%\Scripts\activate.bat %venv_name%
+echo Activating virtual environment...
+%activate_cmd%
 
 echo Virtual environment (%venv_name%) activated
 
-echo Run program
+echo Running program...
 python main.py
+
+if "%OS%"=="Windows_NT" pause
+else read -p "Press [Enter] key to continue..."
--- setup.bat
+++ setup.bat
@@ -1,30 +1,59 @@
 @echo off
-set "miniconda_url=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
-set "miniconda_installer=%~dp0Miniconda3-latest-Windows-x86_64.exe"
 set "python_version=3.9.13"
 set "venv_name=TAN_env"
 
 REM set TAN_dir to current directory
 set "TAN_dir=%~dp0"
 
-REM if not exists anaconda or miniconda, install
-
-if not exist "%UserProfile%\miniconda3" (
-  if not exist "%UserProfile%\anaconda3" (
-    echo Downloading Miniconda...
-    powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%miniconda_url%', '%miniconda_installer%')"
-
-    echo Installing Miniconda...
-    start /wait "" "%miniconda_installer%" /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=%UserProfile%\miniconda3
-  )
+if "%OS%"=="Windows_NT" (
+  set "miniconda_url=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+  set "miniconda_installer=%~dp0Miniconda3-latest-Windows-x86_64.exe"
 ) else (
-    echo Anaconda or Miniconda already installed
+  set "miniconda_url=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+  set "miniconda_installer=%~dp0Miniconda3-latest-MacOSX-x86_64.sh"
 )
 
-if exist "%UserProfile%\anaconda3\Scripts\conda.exe" (
-  set "conda_path=%UserProfile%\anaconda3"
+
+REM if not exists anaconda or miniconda, install
+if "%OS%"=="Windows_NT" (
+  if not exist "%UserProfile%\miniconda3" (
+    if not exist "%UserProfile%\anaconda3" (
+      echo Downloading Miniconda...
+      powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%miniconda_url%', '%miniconda_installer%')"
+
+      echo Installing Miniconda...
+      start /wait "" "%miniconda_installer%" /InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=%UserProfile%\miniconda3
+    )
+  ) else (
+      echo Anaconda or Miniconda already installed
+  )
 ) else (
-  set "conda_path=%UserProfile%\miniconda3"
+  if not exist "$HOME/miniconda3" (
+    if not exist "$HOME/anaconda3" (
+      echo Downloading Miniconda...
+      curl %miniconda_url% -o %miniconda_installer%
+
+      echo Installing Miniconda...
+      chmod +x %miniconda_installer%
+      bash %miniconda_installer% -b -p $HOME/miniconda3
+    )
+  ) else (
+      echo Anaconda or Miniconda already installed
+  )
+)
+
+if "%OS%"=="Windows_NT" (
+  if exist "%UserProfile%\anaconda3\Scripts\conda.exe" (
+    set "conda_path=%UserProfile%\anaconda3"
+  ) else (
+    set "conda_path=%UserProfile%\miniconda3"
+  )
+) else (
+  if exist "$HOME/anaconda3/bin/conda" (
+    set "conda_path=$HOME/anaconda3"
+  ) else (
+    set "conda_path=$HOME/miniconda3"
+  )
 )
 
 if exist "%conda_path%\envs\%venv_name%" (
@@ -32,18 +61,20 @@ if exist "%conda_path%\envs\%venv_name%" (
 ) else (
     echo %venv_name% not found
     echo Creating virtual environment with Python %python_version%...
-    call %conda_path%\Scripts\conda create -n %venv_name% python==%python_version% -y
+    if "%OS%"=="Windows_NT" (
+        call %conda_path%\Scripts\conda create -n %venv_name% python==%python_version% -y
+        set "activate_cmd=call %conda_path%\Scripts\activate.bat %venv_name%"
+    ) else (
+        $conda_path/bin/conda create -n %venv_name% python=%python_version% -y
+        set "activate_cmd=source $conda_path/bin/activate %venv_name%"
     )
-  )
 )
 
 
-REM activate TAN environment
 echo Activating virtual environment... at %conda_path%\envs\%venv_name%
-call %conda_path%\Scripts\activate.bat %venv_name%
+%activate_cmd%
 
 
-REM go to TAN directory, pip install -r -requirements.txt
 echo Installing TAN requirements
 cd %TAN_dir%
 pip install -r requirements.txt
